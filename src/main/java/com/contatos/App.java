c647375c0f11e51cfa3772ec0136a6ee307ae0c7
Iarlen <iarlensilva775@gmail.com>
2017-08-29 19:52:02 -0300

    Melhorando a inicialização

--- src/main/java/com/contatos/App.java
+++ src/main/java/com/contatos/App.java
@@ -1,8 +1,5 @@
 package com.contatos;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,16 +11,7 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 		Propriedades p = new Propriedades();
-		File arquivo = new File(p.getCaminho());
-
-		if (!arquivo.exists()) {
-			try {
-				arquivo.createNewFile();
-			} catch (IOException e) {
-				System.out.println("Erro ao criar o arquivo");
-				e.printStackTrace();
-			}
-		}
+		p.criarArquivo();
 	}
 
 }
